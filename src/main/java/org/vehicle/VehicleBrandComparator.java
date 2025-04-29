8f0c371eabbb4f90bf03c88dec8ad47936c18999
katekiselevich <kiselkath@gmail.com>
2025-04-29 15:24:30 +0200

    Added VechicleBrandComparator.java

--- src/main/java/org/vehicle/VehicleBrandComparator.java
+++ src/main/java/org/vehicle/VehicleBrandComparator.java
@@ -1,8 +1,15 @@
 package org.vehicle;
 
+import java.util.Comparator;
+
 /**
  * Custom comparator to sort by brand
  */
 
-public class VehicleBrandComparator {
+public class VehicleBrandComparator implements Comparator<Vehicle> {
+
+    @Override
+    public int compare(Vehicle v1, Vehicle v2) {
+        return v1.getBrand().compareToIgnoreCase(v2.getBrand()) ;
+    }
 }
