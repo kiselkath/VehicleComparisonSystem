a40379ba5a58a6ac9224471512735c19fadfc24c
katekiselevich <kiselkath@gmail.com>
2025-04-29 16:55:17 +0200

    Added test

--- src/main/java/org/vehicle/VehicleApp.java
+++ src/main/java/org/vehicle/VehicleApp.java
@@ -1,5 +1,9 @@
 package org.vehicle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,25 @@ public class VehicleApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        List<Vehicle> tachki = new ArrayList<>();
+        tachki.add(new Vehicle("Tesla", 2020));
+        tachki.add(new Vehicle("Ford", 2018));
+        tachki.add(new Vehicle("Toyota", 2021));
+        tachki.add(new Vehicle("Honda", 2020));
+
+        System.out.println("====AVAILABLE TACHKI====");
+        tachki.forEach(System.out::println);
+
+        //Sorting
+        // 1 - by year
+        Collections.sort(tachki);
+        System.out.println("Sorted by Year (Newest First == DESC):");
+        tachki.forEach(System.out::println);
+
+        // 2 - by brand
+        tachki.sort(new VehicleBrandComparator());
+        System.out.println("Sorted by Brand (Alphabetical):");
+        tachki.forEach(System.out::println);
+
     }
 }
---
+++ src/test/java/org/vehicle/VehicleTestTest.java
@@ -0,0 +1,46 @@
+package org.vehicle;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleTestTest {
+        private List<Vehicle> tachki;
+
+    @BeforeEach
+    void setUp() {
+        List<Vehicle> tachki = new ArrayList<>();
+        tachki.add(new Vehicle("Tesla", 2020));
+        tachki.add(new Vehicle("Ford", 2018));
+        tachki.add(new Vehicle("Toyota", 2021));
+        tachki.add(new Vehicle("Honda", 2020));
+    }
+
+    @Test
+    @DisplayName("Vehicles should sort by year (newest first)")
+    void testSortByYear() {
+        Collections.sort(tachki);
+
+        assertEquals("BMW", tachki.get(0).getBrand());
+        assertEquals("Toyota", tachki.get(1).getBrand());
+        assertEquals("Honda", tachki.get(2).getBrand());
+        assertEquals("Ford", tachki.get(3).getBrand());
+    }
+
+    @Test
+    @DisplayName("Vehicles should sort by brand name alphabetically")
+    void testSortByBrand() {
+        tachki.sort(new VehicleBrandComparator());
+
+        assertEquals("BMW", tachki.get(0).getBrand());
+        assertEquals("Ford", tachki.get(1).getBrand());
+        assertEquals("Honda", tachki.get(2).getBrand());
+        assertEquals("Toyota", tachki.get(3).getBrand());
+    }
+}
